d399de27f27c883a4b32f3165f2f0c887808ce44
sabbir10s <sabbirahmed1023@gmail.com>
2022-04-18 16:08:53 +0600

    404 not found page done

--- src/Shared/NotFound/NotFound.js
+++ src/Shared/NotFound/NotFound.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import notFound from '../../images/404.png'
 const NotFound = () => {
     return (
-        <div>
-            <img src={notFound} alt="" />
+        <div className='mt-12 text-center h-[80vh] flex justify-center items-center'>
+            <div>
+                <h1 className='text-9xl font-bold text-red-500'>404</h1>
+                <p className='text-3xl font-bold my-5'> Not Found</p>
+                <p className='text-sm'>The resource requested could not be found on this server!</p>
+            </div>
         </div>
     );
 };
