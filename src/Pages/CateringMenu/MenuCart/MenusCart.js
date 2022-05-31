90adf24c6433a59b40f54cd4f0579821d1c724a1
sabbir10s <sabbirahmed1023@gmail.com>
2022-06-01 00:22:02 +0600

    test commit

--- src/Pages/CateringMenu/MenuCart/MenusCart.js
+++ src/Pages/CateringMenu/MenuCart/MenusCart.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const MenusCart = ({ menu }) => {
-    const {id, img, name, price, rating, description } = menu;
+    const { id, img, name, price, rating, description } = menu;
     const navigate = useNavigate();
 
     const handelMenuDetails = id => {
         navigate(`/menudetails/${id}`);
     }
 
-
+    //d
     return (
         <div className='text-center border-1 rounded-lg p-5 shadow-lg hover:shadow-2xl'>
             <img className='mx-auto w-[200px]' src={img} alt="" />
@@ -19,7 +19,7 @@ const MenusCart = ({ menu }) => {
                 <p>Rating: {rating}</p>
                 <p>Details: {description.slice(0, 115)}</p>
             </div>
-            <button onClick={()=>handelMenuDetails(id)} className=' block bg-yellow-500  rounded-full py-2 px-7 font-bold mt-3'>Order Now</button>
+            <button onClick={() => handelMenuDetails(id)} className=' block bg-yellow-500  rounded-full py-2 px-7 font-bold mt-3'>Order Now</button>
         </div>
     );
 };
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_apiKey,
     authDomain: process.env.REACT_APP_authDomain,
