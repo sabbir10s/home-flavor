ff46f6534e09fbd13d834205d9ff4e80adcebcd7
sabbir10s <sabbirahmed1023@gmail.com>
2022-06-02 00:29:38 +0600

    menu cart design done

--- src/Pages/CateringMenu/MenuCart/MenusCart.js
+++ src/Pages/CateringMenu/MenuCart/MenusCart.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import './MenuCart.css'
 const MenusCart = ({ menu }) => {
-    const { id, img, name, price, rating, description } = menu;
+    const { id, img, name, price, rating } = menu;
     const navigate = useNavigate();
 
     const handelMenuDetails = id => {
         navigate(`/menudetails/${id}`);
     }
 
-    //d
     return (
-        <div className='text-center border-1 rounded-lg p-5 shadow-lg hover:shadow-2xl'>
-            <img className='mx-auto w-[200px]' src={img} alt="" />
-            <div className='text-left'>
-                <p className='text-3xl font-bold text-yellow-500'>{name}</p>
-                <p>Price: {price}/-Tk</p>
-                <p>Rating: {rating}</p>
-                <p>Details: {description.slice(0, 115)}</p>
+        <div className='cart border-1 rounded-md shadow-md hover:shadow-xl'>
+            <div>
+                <div className=' bg-slate-200 rounded-md py-[15px]'>
+                    <img className='mx-auto max-w-[200px]' src={img} alt="" />
+                </div>
+                <div className='text-center my-5'>
+                    <p className='text-3xl font-bold text-yellow-500'>{name}</p>
+                    <p>Rating: {rating}</p>
+                    <p>Price: {price}/-Tk</p>
+                </div>
             </div>
-            <button onClick={() => handelMenuDetails(id)} className=' block bg-yellow-500  rounded-full py-2 px-7 font-bold mt-3'>Order Now</button>
+            <div className='cart-button hidden rounded-md'>
+                <button onClick={() => handelMenuDetails(id)} className=' bg-yellow-500  rounded-full py-2 px-7 font-bold'>Order Now</button>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,11 +5,11 @@ import './Banner.css';
 
 const Banner = () => {
     return (
-        <div className='banner my-7'>
-            <div className=' grid md:grid-cols-2'>
+        <div className='banner'>
+            <div className=' grid md:grid-cols-2 mt-5 md:my-7'>
                 <div className='flex justify-center items-center'>
                     <div>
-                        <h3 className='lg:text-7xl md:text-5xl text-4xl mb-3 font-bold font-mono'>I believe good food offer great smile</h3>
+                        <h3 className='text-black leading-normal lg:leading-[90px] lg:text-7xl md:text-5xl text-4xl mb-3 font-bold font-mono'>I believe good food offer great smile</h3>
                         <p className='text-gray-500 mb-3'>'Home Flavor' is a neighborhood home food catering service, serving seasonal global cuisine driven by the faire.</p>
                         <PrimaryButton>Book Now</PrimaryButton>
 
